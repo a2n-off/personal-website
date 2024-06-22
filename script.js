3b35f796348c763f9900082fa3a92941dcee1703
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2024-06-22 17:58:23 +0200

    add media query

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         requestAnimationFrame(raf)
+
+        lenis.scrollTo(0, {immediate: true, force:true}) // on reload the page is at the top
+
         return lenis;
     }
     const lenis = loadLenis();
@@ -142,79 +145,82 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     tooltips();
 
-    // set the bck to dark for avoiding flash at start
-    document.body.style.backgroundColor = 'var(--pri-color)';
+    function loaderAnimation() {
+        // set the bck to dark for avoiding flash at start
+        document.body.style.backgroundColor = 'var(--pri-color)';
 
-    // block the scroll
-    lenis.stop();
+        // block the scroll
+        lenis.stop();
 
-    // define the default duration and delay of all animation
-    const baseDelay = 0.1;
-    const baseDuration = 0.8;
-    const speedDuration = 0.3;
+        // define the default duration and delay of all animation
+        const baseDelay = 0.1;
+        const baseDuration = 0.8;
+        const speedDuration = 0.3;
 
-    // get all the elem for the starting animation
-    const nav = document.getElementById('nav');
-    const loader = document.getElementById('loader');
-    const gridLoader = document.getElementById('grid-loader');
-    const nbOfCol = window.getComputedStyle(gridLoader).getPropertyValue('grid-template-columns').split(' ').length;
-    const name = document.getElementById('name');
-    const lastname = document.getElementById('lastname');
+        // get all the elem for the starting animation
+        const nav = document.getElementById('nav');
+        const loader = document.getElementById('loader');
+        const gridLoader = document.getElementById('grid-loader');
+        const nbOfCol = window.getComputedStyle(gridLoader).getPropertyValue('grid-template-columns').split(' ').length;
+        const name = document.getElementById('name');
+        const lastname = document.getElementById('lastname');
 
-    let tlmain = gsap.timeline();
-    let tla = gsap.timeline();
+        let tlmain = gsap.timeline();
+        let tla = gsap.timeline();
 
-    /* add the col and animate the entrance */
-    for (let i = 0; i < nbOfCol; i++) {
-        const loaderCol = document.createElement('div');
-        loaderCol.classList.add('loader-col');
-        loaderCol.classList.add(`c${i}`);
-        const newChildRef = gridLoader.appendChild(loaderCol);
-        tla.to(newChildRef, {height: '100%', duration: baseDuration}, (baseDelay/2)*i);
+        /* add the col and animate the entrance */
+        for (let i = 0; i < nbOfCol; i++) {
+            const loaderCol = document.createElement('div');
+            loaderCol.classList.add('loader-col');
+            loaderCol.classList.add(`c${i}`);
+            const newChildRef = gridLoader.appendChild(loaderCol);
+            tla.to(newChildRef, {height: '100%', duration: baseDuration}, (baseDelay/2)*i);
+        }
+
+        /* setup the text */
+        name.innerHTML = name.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        name.style.color = 'var(--sec-color)';
+        name.style.zIndex = '1';
+
+        lastname.innerHTML = lastname.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        lastname.style.color = 'var(--sec-color)';
+        lastname.style.zIndex = '1';
+
+        /* animate  the text */
+        let tlb = gsap.timeline();
+        name.childNodes.forEach((e, i) => {
+            if (i === 0) {
+                tlb.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'});
+            } else {
+                tlb.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, `<${0.03}`);
+            }
+        })
+
+        let tlc = gsap.timeline();
+        lastname.childNodes.forEach((e, i) => {
+            if (i === 0) {
+                tlc.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'});
+            } else {
+                tlc.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, `<${0.03}`);
+            }
+        })
+
+        /* fade all the elements */
+        let tld = gsap.timeline();
+        tld.to(loader, {autoAlpha: 0, duration: baseDuration*3, ease: 'expoScale(0.5,7,none)', onComplete: () => {
+                // reset all the main element
+                nav.style.zIndex = 2; /* no idea why I need this to have my blur working but since it work ... */
+                lenis.start();
+                loader.remove();
+            }}, 'dstart')
+        tld.to(name, {color: '#000', duration: baseDuration*3, ease: 'expoScale(0.5,7,none)'}, 'dstart'); // HACK the color anim doesn't work w/ var ...
+        tld.to(lastname, {color: '#000', duration: baseDuration*3, ease: 'expoScale(0.5,7,none)'}, 'dstart');
+
+        /* reset the body color */
+        document.body.style.backgroundColor = 'var(--sec-color)';
+        /* add all the secondary timeline to the main */
+        tlmain.add(tla).add(tlb, 'start').add(tlc, 'start').add(tld);
     }
-
-    /* setup the text */
-    name.innerHTML = name.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    name.style.color = 'var(--sec-color)';
-    name.style.zIndex = '1';
-
-    lastname.innerHTML = lastname.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    lastname.style.color = 'var(--sec-color)';
-    lastname.style.zIndex = '1';
-
-    /* animate  the text */
-    let tlb = gsap.timeline();
-    name.childNodes.forEach((e, i) => {
-        if (i === 0) {
-            tlb.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'});
-        } else {
-            tlb.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, `<${0.03}`);
-        }
-    })
-
-    let tlc = gsap.timeline();
-    lastname.childNodes.forEach((e, i) => {
-        if (i === 0) {
-            tlc.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'});
-        } else {
-            tlc.to(e, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, `<${0.03}`);
-        }
-    })
-
-    /* fade all the elements */
-    let tld = gsap.timeline();
-    tld.to(loader, {autoAlpha: 0, duration: baseDuration*3, ease: 'expoScale(0.5,7,none)', onComplete: () => {
-        // reset all the main element
-        loader.remove();
-        lenis.start();
-        nav.style.zIndex = 2; /* no idea why I need this to have my blur working but since it work ... */
-    }}, 'dstart')
-    tld.to(name, {color: 'var(--pri-color)', duration: baseDuration*3, ease: 'expoScale(0.5,7,none)'}, 'dstart');
-    tld.to(lastname, {color: 'var(--pri-color)', duration: baseDuration*3, ease: 'expoScale(0.5,7,none)'}, 'dstart');
-
-    /* reset the body color */
-    document.body.style.backgroundColor = 'var(--sec-color)';
-    /* add all the secondary timeline to the main */
-    tlmain.add(tla).add(tlb, 'start').add(tlc, 'start').add(tld);
+    loaderAnimation();
 
 });
