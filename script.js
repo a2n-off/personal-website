9339c360dfb5477fdcb5ad3ed286cadd83b83379
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2024-06-25 22:33:42 +0200

    add footer

--- script.js
+++ script.js
@@ -275,22 +275,255 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         })
 
-        /* animate the img */
-        const imgs = document.getElementsByClassName('animate-pic');
-        Array.from(imgs).forEach((el, i) => {
-            let tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: el,
-                    start: 'top 90%',
-                    scrub: false,
-                    toggleAction: 'play',
-                    markers: false
-                }
-            });
-            el.childNodes.forEach((e, i) => {
-                tl.to(e, {x: 0, stagger: 0.05, autoAlpha: 1, duration: 0.5, ease: 'expoScale(0.5,7,none)'});
+        /* animate footer */
+        const footer = document.getElementById('grid-footer');
+        const ftl = gsap.timeline({
+            scrollTrigger: {
+                trigger: footer,
+                start: 'top 95%',
+                scrub: false,
+                toggleAction: 'play',
+                markers: false
+            }
+        });
+        ftl.to(footer, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, 'tag');
+        footer.childNodes.forEach((childNode) => {
+            childNode.childNodes.forEach((p) => {
+                p.innerHTML = p.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+                p.childNodes.forEach((span, i) => {
+                    if (i === 0) {
+                        ftl.to(span, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, 'tag');
+                    } else {
+                        ftl.to(span, {y: 0, stagger: 0.05, autoAlpha: 1, duration: speedDuration, ease: 'expoScale(0.5,7,none)'}, `<${0.003}`);
+                    }
+                })
             })
         })
+
+        /* animate the img */
+        const images = document.querySelectorAll('img');
+
+        function loadImage(img) {
+            return new Promise((resolve) => {
+                if (img.complete) {
+                    resolve();
+                } else {
+                    img.onload = () => resolve();
+                    img.onerror = () => resolve(); // Gérer les erreurs de chargement
+                }
+            });
+        }
+
+        function clamp(number, min, max) {
+            return Math.max(min, Math.min(number, max));
+        }
+
+        async function processImages() {
+            for (const img of images) {
+                await loadImage(img);
+
+                // get the img h & w and calculate the aspect
+                const imgWidth = img.width;
+                const imgHeight = img.height;
+                const imgAspect = imgWidth / imgHeight;
+                const imgSrc = img.src;
+
+                // create a canvas w/ the same size of the img
+                const canvas = document.createElement('canvas');
+                canvas.width = imgWidth;
+                canvas.height = imgHeight;
+                canvas.style.visibility = 'hidden';
+                canvas.style.transform = 'translateX(100%)';
+
+                // replace the img by the canva
+                img.parentNode.replaceChild(canvas, img);
+
+                // init the rendering of three.js
+                const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+                renderer.setSize(imgWidth, imgHeight);
+
+                // init scene, camera and light
+                const scene = new THREE.Scene();
+                const camera = new THREE.OrthographicCamera(-imgAspect, imgAspect, 1, -1, 0.1, 10);
+                camera.position.z = 1;
+                const light = new THREE.DirectionalLight(0xffffff, 1);
+                light.position.set(0, 0, 1).normalize();
+                scene.add(light);
+
+                // load the img texture
+                const textureLoader = new THREE.TextureLoader();
+                textureLoader.load(imgSrc, function (imgTexture) {
+                    // create a material from the img
+                    const imgMaterial = new THREE.MeshBasicMaterial({ map: imgTexture });
+
+                    // create a plane geometry of the same size has the canva
+                    const geometry = new THREE.PlaneGeometry(2 * imgAspect, 2);
+
+                    // create mesh and add it to the scene
+                    const mesh = new THREE.Mesh(geometry, imgMaterial);
+                    scene.add(mesh);
+
+                    // Variables for grid and mouse interaction
+                    let gridTexture;
+                    let mouse = { x: 0, y: 0, vX: 0, vY: 0 };
+                    let prevMouse = { x: 0, y: 0 };
+
+                    const settings = {
+                        relaxation: 0.96,
+                        mouse: 0.11,
+                        strength: 0.36,
+                        size: 607
+                    };
+
+                    /**
+                     * generate the grid texture (dataTexture) in function of the parameter
+                     */
+                    function generateGrid() {
+                        const size = settings.size * settings.size;
+                        const data = new Float32Array(4 * size);
+
+                        for (let i = 0; i < size; i++) {
+                            const stride = i * 4;
+                            let r = Math.random() * 255 - 125;
+                            let g = Math.random() * 255 - 125;
+                            let b = r;
+
+                            data[stride] = r; // red, and also X
+                            data[stride + 1] = g; // green, and also Y
+                            data[stride + 2] = b; // blue
+                        }
+
+                        gridTexture = new THREE.DataTexture(data, settings.size, settings.size, THREE.RGBAFormat, THREE.FloatType);
+                        gridTexture.needsUpdate = true;
+                    }
+
+                    /**
+                     * generate the grid texture and add the texture to the scene
+                     */
+                    function addObjects() {
+                        generateGrid();
+                        imgTexture.needsUpdate = true;
+                        const vertexShader = document.getElementById('vertexShader').textContent;
+                        const fragmentShader = document.getElementById('fragmentShader').textContent;
+
+                        const randomMaterial = new THREE.ShaderMaterial({
+                            extensions: {
+                                derivatives: "#extension GL_OES_standard_derivatives : enable"
+                            },
+                            side: THREE.DoubleSide,
+                            uniforms: {
+                                time: {
+                                    value: 0
+                                },
+                                resolution: {
+                                    value: new THREE.Vector4(1, 1, 1, 1)
+                                },
+                                uTexture: {
+                                    value: imgTexture
+                                },
+                                uDataTexture: {
+                                    value: gridTexture
+                                },
+                            },
+                            vertexShader,
+                            fragmentShader
+                        });
+                        const randomGeometry = new THREE.PlaneGeometry(2 * imgAspect, 2);
+                        //const randomGeometry = new THREE.PlaneGeometry(1);
+                        const randomMesh = new THREE.Mesh(randomGeometry, randomMaterial);
+                        scene.add(randomMesh);
+                    }
+
+                    /**
+                     * update the texture in fc of the mouse movement
+                     */
+                    function updateDataTexture() {
+                        let data = gridTexture.image.data;
+
+                        for (let i = 0; i < data.length; i += 4) {
+                            data[i] *= settings.relaxation;
+                            data[i + 1] *= settings.relaxation;
+                        }
+
+                        let gridMouseX = settings.size * mouse.x;
+                        let gridMouseY = settings.size * (1 - mouse.y);
+                        let maxDist = settings.size * settings.mouse;
+                        let aspect = imgHeight / imgWidth;
+
+                        for (let i = 0; i < settings.size; i++) {
+                            for (let j = 0; j < settings.size; j++) {
+
+                                let distance = ((gridMouseX - i) ** 2) / aspect + (gridMouseY - j) ** 2;
+                                let maxDistSq = maxDist ** 2;
+
+                                if (distance < maxDistSq) {
+
+                                    let index = 4 * (i + settings.size * j);
+
+                                    let power = maxDist / Math.sqrt(distance);
+                                    power = clamp(power, 0, 10);
+
+                                    data[index] += settings.strength * 100 * mouse.vX * power;
+                                    data[index + 1] -= settings.strength * 100 * mouse.vY * power;
+                                }
+                            }
+                        }
+
+                        mouse.vX *= 0.9;
+                        mouse.vY *= 0.9;
+                        gridTexture.needsUpdate = true;
+                    }
+
+                    /**
+                     * render all the stuff
+                     */
+                    function render() {
+                        updateDataTexture();
+                        renderer.render(scene, camera);
+                        requestAnimationFrame(render);
+                    }
+
+                    /**
+                     * update the coordinate of the mouse for is position into the canvas
+                     * @param event
+                     */
+                    function updateMousePosition(event) {
+                        const rect = canvas.getBoundingClientRect();
+                        mouse.x = (event.clientX - rect.left) / imgWidth;
+                        mouse.y = (event.clientY - rect.top) / imgHeight;
+
+                        mouse.vX = mouse.x - prevMouse.x;
+                        mouse.vY = mouse.y - prevMouse.y;
+
+                        prevMouse.x = mouse.x;
+                        prevMouse.y = mouse.y;
+                    }
+
+                    canvas.addEventListener('mousemove', event => {
+                        updateMousePosition(event);
+                    });
+
+                    /**
+                     * first add the object then render it
+                     */
+                    let tl = gsap.timeline({
+                        scrollTrigger: {
+                            trigger: canvas,
+                            start: 'top 90%',
+                            scrub: false,
+                            toggleAction: 'play',
+                            markers: false
+                        }
+                    });
+
+                    addObjects();
+                    render(canvas);
+
+                    tl.to(canvas, {x: 0, autoAlpha: 1, stagger: 0.05, duration: 0.5, ease: 'expoScale(0.5,7,none)'});
+                });
+            }
+        }
+        processImages();
 
         /* add all the secondary timeline to the main */
         tlmain
